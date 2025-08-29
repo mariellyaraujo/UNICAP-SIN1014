736b974a0be04462aac980f3dc716a5344f16bcb
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-08-29 19:45:39 -0300

    preenchendo ImpDepartamento

--- atv1/ImpDepartamento.java
+++ atv1/ImpDepartamento.java
@@ -1,5 +1,8 @@
 public class ImpDepartamento {
-    public static void main (String[] args){
-        
+    public static void main(String[] args) {
+        Departamento d = new Departamento(2309, "Operações");
+        Funcionario f = new Funcionario(12345, "Melly", d);
+
+        System.out.print(f.toString());
     }
 }
