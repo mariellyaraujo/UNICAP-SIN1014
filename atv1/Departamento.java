ddce96d83e9acd01b0e91244c00a0ae0ad93d326
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-08-29 19:44:43 -0300

    update departamento

--- atv1/Departamento.java
+++ atv1/Departamento.java
@@ -1,32 +1,39 @@
-Class Departamento {
+class Departamento {
     private int codigo;
     private String nome;
 
     /// get set codigo
-    public void setCodigo(int codigo){
-        if(codigo > 0) this.codigo = codigo;
-        else System.out.println("coloque um codigo maior que 0");
+    public void setCodigo(int codigo) {
+        if (codigo > 0)
+            this.codigo = codigo;
+        else
+            System.out.println("coloque um codigo maior que 0");
     }
 
-    public int getCodigo(){
+    public int getCodigo() {
         return this.codigo;
     }
 
     /// get set nome
-    public void setNome(String nome){
-        if(nome.isEmpty() || nome.isBlank()) this.nome = nome;
-        else System.out.println("coloque um nome valido");
+    public void setNome(String nome) {
+        this.nome = nome;
+        // System.out.println("coloque um nome valido");
 
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
 
     /// construtor
-    public Departamento(int codigo, String nome){
+    public Departamento(int codigo, String nome) {
         setCodigo(codigo);
         setNome(nome);
     }
 
+    /// toString - um método correto de imprimir um objeto
+
+    public String toString() {
+        return "Departamento: \nnome: " + getNome() + "\ncodigo: " + getCodigo();
+    }
 }
