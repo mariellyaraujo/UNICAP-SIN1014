152f621fe3fc7d6778c637ec07073341619dec94
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-08-29 19:43:50 -0300

    preenchendo a classe funcioanrio

---
+++ atv1/Funcionario.java
@@ -0,0 +1,43 @@
+class Funcionario {
+    private int matricula;
+    private String nome;
+    private Departamento depto;
+
+    /// setters
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDepto(Departamento depto) {
+        this.depto = depto;
+    }
+
+    /// getters
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public Departamento getDepto() {
+        return this.depto;
+    }
+
+    /// construtor
+    public Funcionario(int matricula, String nome, Departamento depto) {
+        setMatricula(matricula);
+        setNome(nome);
+        setDepto(depto);
+    }
+
+    /// toString
+    public String toString() {
+        return "Funcionario: \nmatricula: " + getMatricula() + "\nnome: " + getNome() + "\n" + getDepto().toString();
+    }
+}
