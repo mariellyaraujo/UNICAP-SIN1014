b124a1b43737b2ec195ec53735de796e11f49e0e
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-03 21:01:29 -0300

    preechendo ContaPoupanca

--- atvsClasse/atvHeranca/ContaPoupanca.java
+++ atvsClasse/atvHeranca/ContaPoupanca.java
@@ -1,5 +1,26 @@
 package atvsClasse.atvHeranca;
 
-public class ContaPoupanca {
-    
+public class ContaPoupanca extends ContaBancaria{
+    private int diaDeRendimento;
+
+    ///construtor
+    public ContaPoupanca(String cliente, int numConta, double saldo, int diaDeRendimento){
+        super(cliente, numConta, saldo);
+        setDiaDeRendimento(diaDeRendimento);
+    }
+
+    ////fazer os setters e getters do conta bancaria
+    ///set & get
+    public void setDiaDeRendimento(int diaDeRendimento){
+        this.diaDeRendimento = diaDeRendimento;
+    }
+
+    public int getDiaDeRendimento(){
+        return this.diaDeRendimento;
+    }
+
+    public void calcularNovoSaldo(double taxa){
+        double saldo = getSaldo();
+        setSaldo(saldo + saldo*taxa);
+    }
 }
