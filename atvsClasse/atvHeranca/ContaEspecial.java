12dd606aeeed2f57d43081baa01e04303d7f5243
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-03 21:00:24 -0300

    preenchendo ContaEspecial

--- atvsClasse/atvHeranca/ContaEspecial.java
+++ atvsClasse/atvHeranca/ContaEspecial.java
@@ -1,5 +1,18 @@
 package atvsClasse.atvHeranca;
 
-public class ContaEspecial {
-    
+public class ContaEspecial extends ContaBancaria{
+    private double limite;
+
+    public ContaEspecial(double limite, String cliente, int numConta, double saldo){
+        super(cliente, numConta, saldo);
+        setLimite(limite);
+    }
+    ////fazer os setters e getters do conta bancaria
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    public double getLimite(){
+        return this.limite;
+    }
 }
