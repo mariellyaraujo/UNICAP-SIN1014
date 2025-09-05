d609c3e77174c40dda7204ebe4119e5552b404d7
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-05 19:58:50 -0300

    condição cachorro adestrado

--- atvsClasse/atv3/AplicacaoCachorro.java
+++ atvsClasse/atv3/AplicacaoCachorro.java
@@ -7,5 +7,9 @@ public class AplicacaoCachorro {
 
     public static void imprime(Cachorro c){
         System.out.println("Nome: " + c.getNome() + "\nRaça: " + c.getRaca());
+
+        if(c instanceof CachorroAdestrado){
+            System.out.println("Tem frescura?: " + ((CachorroAdestrado)c).getFrescura());
+        }
     }
 }
