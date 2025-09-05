9fbfd7865409b85f110c50e145ea012727d1a0db
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-05 13:05:02 -0300

    preenchendo a classe Filme

--- Lista01/q1/Filme.java
+++ Lista01/q1/Filme.java
@@ -1,5 +1,42 @@
 package Lista01.q1;
 
 public class Filme {
-    
+    private String titulo;
+    private String genero;
+    private int anoLancamento;
+
+    ///Construtores
+    public Filme(String titulo, String genero, int anoLancamento){
+        setTitulo(titulo);
+        setGenero(genero);
+        setAnoLancamento(anoLancamento);
+    }
+    //sobrecarga
+    public Filme(String titulo, String genero){
+        setTitulo(titulo);
+        setGenero(genero);
+        setAnoLancamento(0);
+    }
+
+    ///setters
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+    public void setAnoLancamento(int anoLancamento){
+        this.anoLancamento = anoLancamento;
+    }
+
+    ///getters
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+    public int getAnoLancamento(){
+        return this.anoLancamento;
+    }
 }
