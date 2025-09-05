a19f4aadec6c9caf9e8c0b493b2f11d9aed28b6b
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-05 19:19:59 -0300

    criando o metodo imprime

--- atvsClasse/atv2/Heranca.java
+++ atvsClasse/atv2/Heranca.java
@@ -4,4 +4,15 @@ public class Heranca {
     public static void main (String[] args){
 
     }
+
+    public static void imprime(Cliente c){
+        System.out.println("Nome: " + c.getNome() + "\nEndereço: " + c.getEndereco());
+
+        if(c instanceof ClientePessoaFisica){
+            System.out.println("CPF: " + ((ClientePessoaFisica)c).getCpf());
+        } //else if (c instanceof ClientePessoaJuridica){
+           // System.out.println("CNPJ: " + ((ClientePessoaJuridica)c).getCnpj());
+        //}
+
+    }
 }
