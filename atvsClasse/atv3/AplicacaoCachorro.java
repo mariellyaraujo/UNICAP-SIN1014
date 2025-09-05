c2450f2f8d873a14d982580fa58704bb38f03237
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-05 19:53:33 -0300

    criação do metodo imprime

---
+++ atvsClasse/atv3/AplicacaoCachorro.java
@@ -0,0 +1,11 @@
+package atvsClasse.atv3;
+
+public class AplicacaoCachorro {
+    public static void main(String[] args){
+
+    }
+
+    public static void imprime(Cachorro c){
+        System.out.println("Nome: " + c.getNome() + "\nRaça: " + c.getRaca());
+    }
+}
