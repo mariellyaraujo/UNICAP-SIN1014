2e95a68497903920a7f607fc65545d82debf2e82
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-06 20:25:50 -0300

    preenchendo classe banco

--- Lista01/q2/Banco.java
+++ Lista01/q2/Banco.java
@@ -1,5 +1,23 @@
 package Lista01.q2;
 
 public class Banco {
-    
+    private Cliente clientes[];
+
+    //getter
+    public Cliente[] getClientes(){
+        return this.clientes;
+    }
+
+    //metodos
+    public Cliente[] cadastraCliente(){
+        Cliente c1 = new Cliente("Alan Turing", "1289", 1500, 25896);
+        Cliente c2 = new Cliente("Ada Love", "987654321", 12345);
+
+        clientes = new Cliente[2];
+
+        clientes[0] = c1;
+        clientes[1] = c2;
+
+        return clientes;
+    }
 }
