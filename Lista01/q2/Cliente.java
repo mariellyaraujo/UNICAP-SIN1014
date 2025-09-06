36fa9aedc61622adde88e6a2026dab0fdcfdf5d3
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-06 19:41:55 -0300

    adicionando setters getters e o construtor

--- Lista01/q2/Cliente.java
+++ Lista01/q2/Cliente.java
@@ -6,5 +6,38 @@ public class Cliente {
     private int saldo;
     private int numConta;
 
-    
+    public Cliente(String nome, String cpf, int saldo, int numConta){
+        setNome(nome);
+        setCpf(cpf);
+        setSaldo(saldo);
+        setNumConta(numConta);
+    }
+
+    ///setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public void setSaldo (int saldo){
+        this.saldo = saldo;
+    }
+    public void setNumConta (int numConta){
+        this.numConta = numConta;
+    }
+
+    ///getters
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+    public int getSaldo(){
+        return this.saldo;
+    }
+    public int getNumConta(){
+        return this.numConta;
+    }
 }
