6ec9acadd74c8250bd5585299085d3f33cabdd3d
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-09 19:08:27 -0300

    criando objetos

--- atvsClasse/atvPolimorfismo/Petshop.java
+++ atvsClasse/atvPolimorfismo/Petshop.java
@@ -2,6 +2,12 @@ package atvsClasse.atvPolimorfismo;
 
 public class Petshop {
     public static void main(String[] args){
-        
+        Animal[] animais = new Animal[2];
+
+        Cachorro c = new Cachorro();
+        Gato g = new Gato();
+
+        animais[0] = c;
+        animais[1] = g;
     }
 }
