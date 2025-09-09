e2fa1a0abf20d4c8e0b24150cceaeb974fc0b339
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-09 19:01:06 -0300

    preenchendo a classe animal com metodos

---
+++ atvsClasse/atvPolimorfismo/Animal.java
@@ -0,0 +1,11 @@
+package atvsClasse.atvPolimorfismo;
+
+public class Animal{
+    public String fazerSom(){
+        return "o animal faz algum som";
+    }
+
+    public String dormir(){
+        return "O animal está dormindo";
+    }
+}
