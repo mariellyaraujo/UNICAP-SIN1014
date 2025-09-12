a9b439de12c7877b5d1f77fa721f5ad17fa6b7d7
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-12 19:30:31 -0300

    susbcrevendo o metodo aumentarSalario

--- atvsClasse/atvClasseAbstrata/Programador.java
+++ atvsClasse/atvClasseAbstrata/Programador.java
@@ -4,4 +4,9 @@ public class Programador extends Funcionario{
     public Programador(String nome, double salario){
         super(nome, salario);
     }
+
+    @Override
+    public void aumentaSalario(){
+        setSalario(getSalario() + (getSalario() * 0.20));
+    }
 }
