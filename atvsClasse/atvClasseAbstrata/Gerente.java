61962f829331325b5e0206347471e8e319b0b301
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-12 19:26:27 -0300

    subscrevendo o metodo aumentarSalario

--- atvsClasse/atvClasseAbstrata/Gerente.java
+++ atvsClasse/atvClasseAbstrata/Gerente.java
@@ -4,4 +4,9 @@ public class Gerente extends Funcionario {
     public Gerente (String nome, double salario){
         super(nome, salario);
     }
+
+    @Override
+    public void aumentaSalario(){
+        setSalario(getSalario() + (getSalario() * 0.20));
+    }
 }
