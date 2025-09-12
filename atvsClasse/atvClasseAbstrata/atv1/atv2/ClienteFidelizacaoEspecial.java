79e29de76ec93af81210786d3c31dbf34ffd708d
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-12 19:54:34 -0300

    criando a classe mt grande o nome

---
+++ atvsClasse/atvClasseAbstrata/atv1/atv2/ClienteFidelizacaoEspecial.java
@@ -0,0 +1,7 @@
+package atvsClasse.atvClasseAbstrata.atv1.atv2;
+
+public class ClienteFidelizacaoEspecial extends ClienteFidelizacao{
+    public ClienteFidelizacaoEspecial(String nome, String endereco, double bonus, String validade){
+        super(nome, endereco, bonus, validade);
+    }
+}
