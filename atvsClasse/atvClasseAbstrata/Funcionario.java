2f633e278d55930f2d691f91818e1a1c59bae410
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-12 19:16:53 -0300

    preenchendo a classe funcionario

--- atvsClasse/atvClasseAbstrata/Funcionario.java
+++ atvsClasse/atvClasseAbstrata/Funcionario.java
@@ -1,5 +1,26 @@
 package atvsClasse.atvClasseAbstrata;
 
 public abstract class Funcionario {
-    
+    private String nome;
+    private double salario;
+
+    //// setters
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    /// getters
+    public String getNome(){
+        return this.nome;
+    }
+    public double getSalario(){
+        return this.salario;
+    }
+
+    ///metodos
+    public void aumentaSalario(){
+        
+    }
 }
