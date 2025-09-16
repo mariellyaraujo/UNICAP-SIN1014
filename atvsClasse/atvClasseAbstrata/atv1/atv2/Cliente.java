0d0e1ebe61573df69915f4d72eac81cfc34c1ed0
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-16 19:11:14 -0300

    adicionando o objeto infocliente na classe mãe cliente

--- atvsClasse/atvClasseAbstrata/atv1/atv2/Cliente.java
+++ atvsClasse/atvClasseAbstrata/atv1/atv2/Cliente.java
@@ -3,10 +3,12 @@ package atvsClasse.atvClasseAbstrata.atv1.atv2;
 public abstract class Cliente {
     private String nome;
     private String endereco;
+    private InfoCliente infocliente;
 
-    public Cliente(String nome, String endereco){
+    public Cliente(String nome, String endereco, InfoCliente infocliente){
         setNome(nome);
         setEndereco(endereco);
+        setInfoCliente(infocliente);
     }
 
     public void setNome(String nome){
@@ -15,11 +17,17 @@ public abstract class Cliente {
     public void setEndereco(String endereco){
         this.endereco = endereco;
     }
+    public void setInfoCliente(InfoCliente infocliente){
+        this.infocliente = infocliente;
+    }
 
     public String getNome(){
         return this.nome;
     }
     public String getEndereco(){
         return this.endereco;
+    }
+    public InfoCliente getInfoCliente(){
+        return this.infocliente;
     }
 }
