f5764623211570997e58b3b9b5d7f7a8be751438
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-16 19:54:25 -0300

    construtor e get set de quadrado

--- atvsClasse/atvInterface/Quadrado.java
+++ atvsClasse/atvInterface/Quadrado.java
@@ -3,6 +3,17 @@ package atvsClasse.atvInterface;
 class Quadrado implements FormaGeometrica{
     private double lado;
 
+    public Quadrado(double lado){
+        setLado(lado);
+    }
+
+    public void setLado(double lado){
+        this.lado = lado;
+    }
+    public double getLado(){
+        return this.lado;
+    }
+
     @Override
     public double area(){
         return lado * lado;
