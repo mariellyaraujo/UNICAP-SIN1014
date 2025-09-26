50d65898d6812ae5111ddab3c3f887182d41bdc2
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-25 21:57:50 -0300

    criação da classe zoo

---
+++ Lista02/Zoo.java
@@ -0,0 +1,23 @@
+package Lista02;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Zoo {
+    Scanner s = new Scanner(System.in);
+
+    private ArrayList<Animal> animals;
+
+    public Zoo (ArrayList<Animal> animals){
+        setAnimals(animals); 
+    }
+
+    public void setAnimals(ArrayList<Animal> animals){
+        this.animals = animals;
+    }
+    public ArrayList<Animal> getAnimals(){
+        return this.animals;
+    }
+
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+    }
+}
