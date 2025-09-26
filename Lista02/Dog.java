43f813aafbb9422f2c78ab24714b1d14c191d39f
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-25 21:33:10 -0300

    criação da classe filha dog

---
+++ Lista02/Dog.java
@@ -0,0 +1,16 @@
+package Lista02;
+
+import java.util.ArrayList;
+
+public class Dog extends Animal{
+    public Dog(long id, int age, double weight){
+        super(id, "Buddy", age, weight, new ArrayList<String>() {{
+            add("sniff");
+        }});
+    }
+
+    @Override
+    public String sound(){
+        return "au";
+    }
+}
