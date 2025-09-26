b3c33c6d1c7b148f23be5e956e7271970dcc3438
Marielly de Araújo Silva <marielly.araujosilv@gmail.com>
2025-09-25 21:37:42 -0300

    criação da classe filha bird

---
+++ Lista02/Bird.java
@@ -0,0 +1,17 @@
+package Lista02;
+
+import java.util.ArrayList;
+
+public class Bird extends Animal{
+
+    public Bird(long id, int age, double weight){
+        super(id, "Tweety", age, weight, new ArrayList<String>() {{
+            add("fly");
+        }});
+    }
+
+    @Override
+    public String sound(){
+        return "piu";
+    }
+}
